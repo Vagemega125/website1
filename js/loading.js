6a0469f3a082978484ff9f4affa195ee6ee599db
Vagemega125 <164818945+Vagemega125@users.noreply.github.com>
2025-02-26 17:31:47 +0000

    Wed, Feb 26, 2025, 9:31 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -2,4 +2,14 @@ const LoadText = document.querySelectro('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load =0
-LoadText.innerText = ``
+
+let int = setInterval(blurring, 300)
+
+function blurring(){
+    load = load+1
+    if (load >99){
+        clearInterval(int)
+    }
+    LoadText.innerText = `${load}%`
+bg.style.filter = "blur(${50 - (load/2)}px)"
+}
