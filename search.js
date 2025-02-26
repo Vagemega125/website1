39a852c32bb4d90a2698d955c6e01daa15f1d0e7
Vagemega125 <164818945+Vagemega125@users.noreply.github.com>
2025-02-26 16:53:46 +0000

    Wed, Feb 26, 2025, 8:53 AM -08:00

---
+++ search.js
@@ -0,0 +1,11 @@
+// palce holder
+
+const search = document.querySelector('.seearch')
+const btn = document.querySelector('.btn')
+// const input = document.querySelector('.input')
+
+btn.addEventListener('click',()=>{
+
+    search.classList.toggle('active')
+    inout.focus()
+})
