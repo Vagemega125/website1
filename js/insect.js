1977586d1d0401ce6c9778ce732b7f2633e41faf
Vagemega125 <164818945+Vagemega125@users.noreply.github.com>
2025-03-31 06:48:04 +0000

    Sun, Mar 30, 2025, 11:48 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,95 +1,95 @@
-const screens =document.querySelectorAll('screen')
-const start_btn = document.getElementById('start-btn')
+const screens = document.querySelectorAll('.screen')
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-const game_container=document.getElementById('game-container')
-const timeEl=document.getElementById('time')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 
-let seconds=0
-let score=0
+let seconds = 0
+let score = 0
 let selected_insect = {}
 
-start_btn.addEventListener('click',()=>{
+start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
 })
 
-choose_insect_btns.forEach(btn=>{
-    btn.addEventListener('click',()=>{
-        screens[1].classList.add('up')
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
         const img = btn.querySlector('img')
         const src = img.getAttribute('src')
         const alt = img.getAttribute('alt')
+        screens[1].classList.add('up')
         selected_insect = {src,alt}
+        screens[1].classList.add('up')
+
+        setTimeout(createInsect, 1000)
         startGame()
-        createInsect()
+        // createInsect()
     })
 })
 
-
-function startGame(){
-    setInterval(increaseTime,1000)
-}
-
-
-function increaseTime(){
-    let s = seconds %60
-    let m = Math.floor(seconds / 60)
-    if (s<10){
-        s = '0${s}'
-    }
-    if (m<10){
-        m = '0${s}'
-    }
-    timeEl.innerHtml ='Time:${m}:${s}'
-    seconds=seconds+1;
-}
-
-
-function createInsect(){
-    const insect = document,createElement('div')
+function createInsect() {
+    const insect = document.createElement('div')
     insect.classList.add('insect')
     insect.innerHTML = '<img src= "${selected_insect.src}" alt = "${selected_insect.alt}" >'
     const {x,y} = getRandomLocation()
     insect.style.top = '${y}px'
-    insect.style.left='${x}px'
+    insect.style.left = '${x}px'
     game_container.appendChild(insect)
-
-    insect.addEventListener('click',()=>{
-        score = score + 1
-        scoreEl.innerHTML='Score: ${score}'
-        insect.addEventListener('click',cathcInsect)
-    })
-
-
-    function catchInsect(){
-        increaseScore()
-        this.classList.add('caught')
-        setTimeout(()=> this.remove(),2000)
-        addInsect()
-    }
-
-    function increaseScore(){
-        score = score + 1
-        scoreEl.HTML = 'Score:${score}'
+    function increaseTime() {
+        let s = seconds % 60
+        let m = Math.floor(seconds / 60)
+        if (s < 10) {
+            s = '0${s}'
+        }
+        if (m < 10) {
+            m = '0${m}'
+        }
+        timeEl.innerHtml = 'Time:${m}:${s}'
+        seconds = seconds + 1;
     }
 }
 
-function addInsects(){
-    setTimeout(createInsect,1000)
-    setTimeout(createInsect,1500)
+function increaseScore() {
+    score = score + 1
+    if (score == 10) {
+        message.classList.add('visible')
+    }
+    scoreEl.HTML = 'Score:${score}'
 }
 
 
-function getRandomLocation(){
+function addInsects() {
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
+}
+
+
+function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
     console.log(Math.random())
-    const x = Math.random()* width
-    const y = Math.random() * height
-    return {x,y}
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return {
+        x,
+        y
+    }
 }
 
 
-.insect.caught()
+function catchInsect() {
+    let insect = event.target;
+    increaseScore()
+    insect.classList.add('caught')
+    setTimeout(() => this.remove(), 2000)
+    addInsect()
+    insect.addEventListener('click', () => {
+        score = score + 1
+        scoreEl.innerHTML = 'Score: ${score}'
+        insect.addEventListener('click', cathcInsect)
+    })
+}
+
 
